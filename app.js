00915b9a0dafbd263785b6812f17318df302e4b4
Jason Schmidt <Zannalov@GMail.com>
2013-02-08 22:16:20 -0500

    WIP

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 ////////////////////////////////////////////////////////////////////////////////
+// Notes
+////////////////////////////////////////////////////////////////////////////////
+
+//  Two parts:
+//      Part 1: Site scraper
+//          For each stub
+//          For each page
+//              If there are still pages, start fetch of next page
+//          For each kickstarter (if not seen)
+//          Get project page
+//          Index
+//      Part 2: RSS feed
+//          Order by date indexed followed by end date ascending
+
+////////////////////////////////////////////////////////////////////////////////
 // Modules
 ////////////////////////////////////////////////////////////////////////////////
 
+var events = require('events');
 var fs = require( 'fs' );
 var http = require( 'http' );
 var jquery = require( 'jquery' );
-var jsdom = require('jsdom').jsdom;
+var jsdom = require('jsdom');
+var sys = require('sys');
 
 ////////////////////////////////////////////////////////////////////////////////
 // Config
 ////////////////////////////////////////////////////////////////////////////////
 
+var configFile = 'test.json';
 var testMode = true;
 var basePath = 'http://www.kickstarter.com';
+var projectPrefix = 'http://www.kickstarter.com/projects/';
 var stubs = [
     '/discover/recommended',
     '/discover/recently-launched',
@@ -23,6 +42,11 @@ var stubs = [
     '/discover/categories/comics/popular',
     '/discover/categories/comics/recommended',
 ];
+
+jsdom.defaultDocumentFeatures = {
+    FetchExternalResources: [],
+    ProcessExternalResources: false
+};
 
 ////////////////////////////////////////////////////////////////////////////////
 // Debug code to serve sample files
@@ -51,7 +75,6 @@ if( testMode ) {
 
         basePath = 'http://localhost:8000/samples';
         stubs = [
-            '/discover/recommended',
             '/discover/ending-soon',
         ];
     })();
@@ -61,8 +84,26 @@ if( testMode ) {
 // Fetch a single page
 ////////////////////////////////////////////////////////////////////////////////
 
+function Page() {
+    this.fetch = this.fetch.bind( this );
+    this.fetchPage = this.fetchPage.bind( this );
+
+    this.projects = [];
+
+    events.EventEmitter.call( this );
+
+    return this;
+}
+sys.inherits( Page , events.EventEmitter );
+
+Page.prototype.basePath = null;
+Page.prototype.stub = null;
+Page.prototype.page = 1;
+Page.prototype.rawResult = null;
+
+// url must be fully qualified
 // callback( err , data )
-function fetchPage( url , callback ) {
+Page.prototype.fetchPage = function( url , callback ) {
     var req = http.request( url , function( res ) {
         // If the status code is outside the 200 range
         if( 200 > res.statusCode || 299 < res.statusCode ) {
@@ -78,32 +119,130 @@ function fetchPage( url , callback ) {
         res.setEncoding( 'utf8' );
         res.on( 'data' , function( chunk ) {
             body += chunk;
-        } );
+        }.bind( this ) );
         res.on( 'end' , function() {
             if( callback ) {
                 callback( null , body );
                 callback = null;
             }
-        } );
-    } );
+        }.bind( this ) );
+    }.bind( this ) );
     req.on( 'error' , function( e ) {
         if( callback ) {
             callback( e , null );
             callback = null;
         }
-    } );
+    }.bind( this ) );
     req.end();
-}
+};
 
-fetchPage( basePath + stubs[0] + '?page=1' , function( err , data ) {
-    if( err ) {
-        console.log( err );
-        return;
+Page.prototype.fetch = function() {
+    this.fullUrl = this.basePath + this.stub + '?page=' + this.page;
+    this.fetchPage( this.fullUrl , function( err , data ) {
+        if( err ) {
+            console.log( 'Error loading (' + this.fullUrl + '): ' + err );
+            return;
+        }
+
+        this.rawResult = data;
+
+        var doc = jsdom.jsdom( data );
+        var window = doc.createWindow();
+        var jq = jquery.create( window );
+
+        var projectsJq = jq( 'li.project' );
+        var parsedProjects = [];
+        projectsJq.each( function( index , item ) {
+            var project = {};
+
+            var projectLink = jq( 'h2 a' , item );
+            if( projectLink.length ) {
+                project.url = projectLink.prop( 'href' );
+                project.name = projectLink.text();
+
+                if( 'https' === project.url.substr( 0 , 5 ) ) {
+                    project.url = 'http' + project.url.substr( 5 );
+                }
+
+                if( project.url.substr( 0 , projectPrefix.length ) === projectPrefix ) {
+                    var str = project.url;
+
+                    // Strip off the prefix
+                    str = str.substr( projectPrefix.length );
+
+                    // Strip out query parameters
+                    if( -1 !== str.indexOf( '?' ) ) {
+                        str = str.substr( 0 , str.indexOf( '?' ) );
+                    }
+
+                    // Split on the first slash
+                    str = str.split( '/' )[0];
+
+                    project.creatorId = str;
+                }
+            }
+
+            var authorSpan = jq( 'h2 span' , item );
+            if( authorSpan.length ) {
+                var author = authorSpan.text();
+                author = author.replace( /^\s+/ , '' );
+                author = author.replace( /\s+$/ , '' );
+                author = author.replace( /\n/ , ' ' );
+                author = author.replace( /\s+/ , ' ' );
+                if( /^by /.test( author ) ) {
+                    author = author.replace( /^by / , '' );
+                    project.creatorName = author;
+                }
+            }
+
+            var projectPhoto = jq( '.projectphoto-little' , item );
+            if( projectPhoto.length && projectPhoto.prop( 'src' ) ) {
+                project.thumbnail = projectPhoto.prop( 'src' );
+            }
+
+            parsedProjects.push( project );
+            this.projects.push( project );
+        }.bind( this ) );
+
+        this.emit( 'pageProjectsLoaded' , parsedProjects );
+    }.bind( this ) );
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Main
+////////////////////////////////////////////////////////////////////////////////
+
+var test = new Page();
+test.basePath = basePath;
+test.stub = stubs[0];
+test.page = 1;
+test.fetch();
+test.on( 'pageProjectsLoaded', function( projects ) {
+    if( projects.length ) {
+        test.page += 1;
+        test.fetch();
+    } else {
+        test.emit( 'allPagesLoaded' );
     }
-
-    var doc = jsdom( data );
-    var window = doc.createWindow();
-    var jq = jquery.create( window );
-
-    console.log( jq( 'li.project' ).length );
 } );
+test.on( 'allPagesLoaded' , function() {
+    console.log( test.projects.length );
+} );
+
+/*
+<rss version="2.0">
+  <channel>
+    <title>STUB_TITLE</title>
+    <link>STUB_LINK</link>
+    <description>STUB_DESCRIPTION</description>
+    <item>
+      <title>PROJECT_TITLE</title>
+      <link>PROJECT_LINK</link>
+      <enclosure url="THUMBNAIL_URL" length="BYTES" type="MIME_TYPE"></enclosure>
+      <pubDate>DATE</pubDate>
+    </item>
+    ...
+  </channel>
+  ...
+</rss>
+*/
