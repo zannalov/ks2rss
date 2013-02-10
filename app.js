f14b5ed2b958b1eaa642446da0436c5224124912
Jason Schmidt <Zannalov@GMail.com>
2013-02-09 23:03:51 -0500

    WIP

--- app.js
+++ app.js
@@ -13,19 +13,19 @@ var sys = require('sys');
 // Config
 ////////////////////////////////////////////////////////////////////////////////
 
-var pageLimit = 30;
-var dataFile = 'projectsSeen.json';
-var outFile = 'ks2rss3.xml';
+var pageLimit = null;
+var dataFile = 'projectsSeen5.json';
+var outFile = 'ks2rss5.xml';
 var testMode = false;
 var basePath = 'http://www.kickstarter.com';
 var projectPrefix = 'http://www.kickstarter.com/projects/';
 var stubs = {
-    'Recommended': '/discover/recommended',
+    //'Recommended': '/discover/recommended',
     'Recently Launched': '/discover/recently-launched',
     'Ending Soon': '/discover/ending-soon',
-    'Small Projects': '/discover/small-projects',
-    'Comics (Popular)': '/discover/categories/comics/popular',
-    'Comics (Recommended)': '/discover/categories/comics/recommended',
+    //'Small Projects': '/discover/small-projects',
+    //'Comics (Popular)': '/discover/categories/comics/popular',
+    //'Comics (Recommended)': '/discover/categories/comics/recommended',
 };
 
 jsdom.defaultDocumentFeatures = {
@@ -70,7 +70,7 @@ if( testMode ) {
 // Fetch a single page
 ////////////////////////////////////////////////////////////////////////////////
 
-function Page() {
+function Loader() {
     this.fetch = this.fetch.bind( this );
     this.fetchPage = this.fetchPage.bind( this );
 
@@ -80,16 +80,16 @@ function Page() {
 
     return this;
 }
-sys.inherits( Page , events.EventEmitter );
+sys.inherits( Loader , events.EventEmitter );
 
-Page.prototype.basePath = null;
-Page.prototype.stub = null;
-Page.prototype.page = 1;
-Page.prototype.rawResult = null;
+Loader.prototype.basePath = null;
+Loader.prototype.stub = null;
+Loader.prototype.page = 1;
+Loader.prototype.rawResult = null;
 
 // url must be fully qualified
 // callback( err , data )
-Page.prototype.fetchPage = function( url , callback ) {
+Loader.prototype.fetchPage = function( url , callback ) {
     var req = http.request( url , function( res ) {
         // If the status code is outside the 200 range
         if( 200 > res.statusCode || 299 < res.statusCode ) {
@@ -122,13 +122,14 @@ Page.prototype.fetchPage = function( url , callback ) {
     req.end();
 };
 
-Page.prototype.fetch = function() {
-    this.fullUrl = this.basePath + this.stub + '?page=' + this.page;
+Loader.prototype.fetch = function() {
+    var page = this.page;
+    this.fullUrl = this.basePath + this.stub + '?page=' + page;
     this.fetchPage( this.fullUrl , function( err , data ) {
         var parsedProjects = [];
         if( err ) {
             console.log( 'Error loading (' + this.fullUrl + '): ' + err );
-            this.emit( 'pageProjectsLoaded' , parsedProjects );
+            this.emit( 'pageProjectsLoaded' , parsedProjects , page );
             return;
         }
 
@@ -149,6 +150,12 @@ Page.prototype.fetch = function() {
                 project.url = projectLink.prop( 'href' );
                 project.name = projectLink.text();
 
+                // Strip out query parameters
+                if( -1 !== project.url.indexOf( '?' ) ) {
+                    project.url = project.url.substr( 0 , project.url.indexOf( '?' ) );
+                }
+
+                // Convert https to http
                 if( 'https' === project.url.substr( 0 , 5 ) ) {
                     project.url = 'http' + project.url.substr( 5 );
                 }
@@ -187,7 +194,14 @@ Page.prototype.fetch = function() {
             var projectPhoto = jq( '.projectphoto-little' , item );
             if( projectPhoto.length && projectPhoto.prop( 'src' ) ) {
                 project.thumbnail = projectPhoto.prop( 'src' );
+
+                // Convert https to http
+                if( 'https' === project.thumbnail.substr( 0 , 5 ) ) {
+                    project.thumbnail = 'http' + project.thumbnail.substr( 5 );
+                }
             }
+
+            project.blurb = jq( '.bbcard_blurb' , item ).text();
 
             if( project.url && project.name && project.thumbnail ) {
                 parsedProjects.push( project );
@@ -195,7 +209,7 @@ Page.prototype.fetch = function() {
             }
         }.bind( this ) );
 
-        this.emit( 'pageProjectsLoaded' , parsedProjects );
+        this.emit( 'pageProjectsLoaded' , parsedProjects , page );
     }.bind( this ) );
 };
 
@@ -203,32 +217,40 @@ Page.prototype.fetch = function() {
 // Main
 ////////////////////////////////////////////////////////////////////////////////
 
-var pastData = JSON.parse( fs.readFileSync( dataFile ) );
+var pastData;
+
+try {
+    pastData = JSON.parse( fs.readFileSync( dataFile ) );
+} catch( e ) {
+    pastData = {};
+}
+
 pastData = pastData || {};
 pastData.stubs = pastData.stubs || {};
-pastData.projectsByStub = pastData.projectsByStub || {};
+pastData.projectsByUrl = pastData.projectsByUrl || {};
 
 var channels = {};
 var stub;
 for( var stubName in stubs ) {
     (function( stub , stubName ) {
-        var page = new Page();
-        page.basePath = basePath;
-        page.stub = stub;
-        page.page = 1;
-        page.fetch();
-        page.on( 'pageProjectsLoaded', function( projects ) {
-            console.log( stub + ' successfully parsed ' + projects.length + ' projects on page ' + page.page );
-            if( projects.length && page.page < pageLimit ) {
-                page.page += 1;
-                page.fetch();
-            } else {
-                page.emit( 'allPagesLoaded' );
+        var loader = new Loader();
+        loader.basePath = basePath;
+        loader.stub = stub;
+        loader.page = 1;
+        loader.fetch();
+        loader.on( 'pageProjectsLoaded', function( projects , pageLoaded ) {
+            if( pageLoaded == loader.page ) {
+                if( projects.length && ( null === pageLimit || loader.page < pageLimit ) ) {
+                    loader.page += 1;
+                    loader.fetch();
+                } else {
+                    loader.emit( 'allPagesLoaded' );
+                }
             }
         } );
-        page.on( 'allPagesLoaded' , function() {
-            console.log( stub + ' successfully parsed ' + page.projects.length + ' projects' );
-            channels[stub] = page.projects;
+        loader.on( 'allPagesLoaded' , function() {
+            console.log( stub + ' successfully parsed ' + loader.projects.length + ' projects' );
+            channels[stub] = loader.projects;
             if( allChannelsLoaded() ) {
                 produceXml();
             }
@@ -262,54 +284,58 @@ function projectXml( project ) {
     xml += '      <title>' + escapeXml( project.name ) + '</title>\n';
     xml += '      <guid>' + escapeXml( project.url ) + '</guid>\n';
     xml += '      <link>' + escapeXml( project.url ) + '</link>\n';
-    xml += '      <description>' + escapeXml( '<a href="' + escapeXml( project.url ) + '">' + escapeXml( project.name ) + '</a><br /><a href="' + escapeXml( project.url ) + '"><img src="' + escapeXml( project.thumbnail ) + '" />' ) + '</description>\n';
+    xml += '      <description>' + escapeXml(
+        '<a href="' + escapeXml( project.url ) + '"><img src="' + escapeXml( project.thumbnail ) + '" /><br />'
+        + '<a href="' + escapeXml( project.url ) + '">' + escapeXml( project.name ) + '</a><br />'
+        + 'By ' + escapeXml( project.creatorName ) + '</a><br />'
+        + '<br />'
+        + escapeXml( project.blurb ) + '<br />'
+    ) + '</description>\n';
     xml += '      <pubDate>' + escapeXml( d.toString() ) + '</pubDate>\n';
     xml += '    </item>\n';
+
     return xml;
 }
 
 function produceXml() {
-    var xml = '<rss version="2.0">\n';
+    var newOnes;
+    var project;
+    var projectIndex;
+    var stub;
+    var stubName;
+    var xml;
+
+    // Start XML
+    xml = '<rss version="2.0">\n';
     xml += '  <channel>\n';
     xml += '    <title>KS2RSS</title>\n';
     xml += '    <link>http://www.kickstarter.com</link>\n';
     xml += '    <description></description>\n';
 
-    var stubName;
-    var stub;
-    var projectIndex;
-    var seen;
-    var project;
-    var newOnes = 0;
+    // Mark all the past projects as seen and re-add them to the XML
+    for( projectIndex in pastData.projectsByUrl ) {
+        // Item XML
+        xml += projectXml( pastData.projectsByUrl[ projectIndex ] );
+    }
+
+    // For each stub
+    newOnes = 0;
     for( stubName in stubs ) {
         stub = stubs[stubName];
-        seen = {}; // Reset for each stub
 
         // Initialize containers/notes if not present
         pastData.stubs[stubName] = stub;
-        pastData.projectsByStub[stub] = pastData.projectsByStub[stub] || [];
 
-        // Mark all the past projects as seen and re-add them to the XML
-        for( projectIndex = 0 ; projectIndex < pastData.projectsByStub[stub].length ; projectIndex ++ ) {
-            project = pastData.projectsByStub[stub][projectIndex];
-
-            // Mark as seen
-            seen[ project.url ] = true;
-
-            // Item XML
-            xml += projectXml( project );
-        }
-
+        // For each project for this stub
         for( projectIndex = 0 ; projectIndex < channels[stub].length ; projectIndex ++ ) {
             project = channels[stub][projectIndex];
 
             // If we've not seen this project before
-            if( ! seen[ project.url ] ) {
-                // Push the project onto the past list
-                pastData.projectsByStub[stub].push( project );
+            if( ! pastData.projectsByUrl[ project.url ] ) {
+                console.log( 'New one: ' + project.name.replace( /\n.*/ , '' ).replace( /^\s+/ , '' ).replace( /\s+$/ , '' ) );
 
-                // Mark it seen
-                seen[ project.url ] = true;
+                // Push the project onto the past list
+                pastData.projectsByUrl[ project.url ] = project;
 
                 // Item XML
                 xml += projectXml( project );
@@ -318,15 +344,17 @@ function produceXml() {
                 newOnes += 1;
             }
         }
-
-        // Close XML
     }
+
+    // Close XML
     xml += '  </channel>\n';
     xml += '</rss>\n';
 
+    // Write results
     fs.writeFileSync( outFile , xml );
-    fs.writeFileSync( dataFile , JSON.stringify( pastData ) );
+    fs.writeFileSync( dataFile , JSON.stringify( pastData , null , '\t' ) );
 
+    // Exit cleanly
     console.log( 'New ones: ' + newOnes );
     process.exit(0);
 }
