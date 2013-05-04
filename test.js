4edbb4147a39a68d67f11b29be21f23c0b5ac8f3
Jason Schmidt <Zannalov@GMail.com>
2013-05-04 11:44:39 -0400

    WIP

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
+/*
 var fetchPage = require( './lib/fetch' ).fetchPage;
-var parseProjectListPage = require( './lib/parseProjectListPage' ).parseProjectListPage;
+var parseProjectListPage = require( './lib/parse' ).parseProjectListPage;
 
 var url = 'http://www.kickstarter.com/discover/recently-launched?ref=sidebar';
-//var url = 'http://www.kickstarter.com/discover/recently-launched?ref=sidebar&page=%%PAGE%%';
 
 fetchPage({
     url: url,
@@ -14,3 +14,26 @@ fetchPage({
         }
     },
 });
+*/
+
+var Fetch = require( './lib/fetch' );
+var ch = require('ch').ch;
+
+ch.setMax( 256 );
+
+Fetch.fetchAllProjectListPages( {
+    urlTemplate: 'http://www.kickstarter.com/discover/recently-launched?ref=sidebar&page=%%page%%',
+
+    eachProjectLoaded: function( project , callback ) {
+        // console.log( 'project fully loaded:' );
+        // console.log( project );
+        // console.log( '' );
+
+        // Randomly choose to keep/discard each project
+        callback( Boolean( Math.round( Math.random() ) ) );
+    },
+
+    allProjectsLoaded: function( projects ) {
+        console.log( 'all projects loaded, total = ' + projects.length );
+    },
+} );
